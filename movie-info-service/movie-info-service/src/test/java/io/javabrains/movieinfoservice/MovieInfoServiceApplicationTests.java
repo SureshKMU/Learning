29a6d941b98845ede65e569e6ff1cf224bd609c6
Suresh Vaikuntam <vaikundamsuresh@gmail.com>
2022-05-14 18:10:44 +0530

    Sample Test Push

---
+++ movie-info-service/movie-info-service/src/test/java/io/javabrains/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
